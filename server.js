bf0c510caf752ab7c226dc8168dda768fa493bf1
Manjunath Kantigavi <mkantigavi@gmail.com>
2025-03-25 14:16:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json()); // Parse JSON request body
 // Update CORS configuration
 app.use(cors({
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500','https://manjunath-kantigavi.github.io'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
